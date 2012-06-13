aa3d6ea8f937af49fd2997d9b54f5150cebc5acc
Jonan Scheffler <jonanscheffler@gmail.com>
2012-06-13 12:25:44 -0400

    Once more with Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThinAnalytics::Application.config.secret_token = 'e260a5a79a424255fc1af4df7096713dc4f7a00b354a25591a622b8c18a60b4b4617da2ad068769f75938d0877c2001f82b78e79dad9cb077366f823071ed4d9'
