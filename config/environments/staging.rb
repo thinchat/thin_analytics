ca761d34e367489ec59c1ca0a93df84031413a41
Chris Anderson <christopher.anderson@gmail.com>
2012-06-17 19:38:46 -0400

    dirty fix

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -43,8 +43,11 @@ ThinAnalytics::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = "http://#{Rails.root}/analytics"
-
+  if Rails.env == "production"
+    config.action_controller.asset_host = "http://thinchat.com/analytics"
+  elsif Rails.env == "staging"
+    config.action_controller.asset_host = "http://staging.thinchat.com/analytics"
+  end
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
