e962324f66ec989962a6cf52ffff8143080dcd0a
Chris Anderson <christopher.anderson@gmail.com>
2012-06-17 19:36:35 -0400

    checking asset host

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -43,7 +43,7 @@ ThinAnalytics::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "http://#{Rails.root}/analytics"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
